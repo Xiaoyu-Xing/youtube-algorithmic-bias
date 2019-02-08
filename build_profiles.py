63df77e9e439e887aa17db0b31b6ff01ed04f9bd
Xiaoyu Xing <xingxy91@gmail.com>
2019-02-07 23:59:38 -0600

    Finish related profiles

--- build_profiles.py
+++ build_profiles.py
@@ -2,6 +2,8 @@ import os
 import random
 import json
 from pprint import pprint
+import math
+import sys
 
 
 class Build_Profiles:
@@ -20,6 +22,8 @@ class Build_Profiles:
             os.makedirs(self.output_path)
         # Average profile details, keyed with last word of each json profile filename
         self.profiles = {}
+        self.profiles_norm_ratio = {}
+        self.profiles_norm_ratio_N_diversity = {}
         # *_dirs are the root directories path for files of profiles, base/related videos
         self.profiles_dir, self.videos_base_dir, self.videos_related_dir = '', '', ''
         # Names of files are stored separately, used together with dir path above
@@ -34,6 +38,8 @@ class Build_Profiles:
         self._parse_folders()
         # Run to generate profile details
         self._generate_profiles()
+        # Profile video upper bound
+        self.limit = 50
 
     # Parse data folder
     def _parse_folders(self):
@@ -93,17 +99,13 @@ class Build_Profiles:
         return videos_short, videos_details
 
     # Collect all the base videos for all profiles
-    def _build_profiles_base(self, limit):
-        '''
-        Input:
-            limit: int, how many top videos to choose
-        '''
+    def _build_profiles_base(self):
         for file in self.videos_base_files:
             path = os.path.join(self.videos_base_dir, file)
             v, v_d = self._load_videos(path)
             key = os.path.splitext(file)[0]
-            self.base_videos[key] = v[:limit]
-            self.base_videos_details[key] = v_d[:limit]
+            self.base_videos[key] = v[:self.limit]
+            self.base_videos_details[key] = v_d[:self.limit]
             self.base_videos_summary[key] = len(self.base_videos[key])
 
     # Give data and file_path (need include .json extension), write file, overwrite if exists.
@@ -128,8 +130,9 @@ class Build_Profiles:
             details: boolean, whether to output detailed version
             limit: int, how many to output
         '''
+        self.limit = limit
         # Run to generate base profile videos
-        self._build_profiles_base(limit)
+        self._build_profiles_base()
         out_path = self.output_path
         for file, videos in self.base_videos.items():
             name_base = 'base_videos_' + file + '.json'
@@ -172,15 +175,64 @@ class Build_Profiles:
             videos_details.append({video['url']: details})
         return videos_short, videos_details
 
-    def _build_profiles_related(self, limit):
+    def _build_profiles_related(self):
         for file in self.videos_related_files:
             key = os.path.splitext(file)[0]
             path = os.path.join(self.videos_related_dir, file)
             v, v_d = self._load_related_videos(path)
-            self.related_videos[key] = v[:limit]
-            self.related_videos_details[key] = v_d[:limit]
+            self.related_videos[key] = v
+            self.related_videos_details[key] = v_d
             self.related_videos_summary[key] = len(self.related_videos[key])
 
-    def output_profiles_related(self, shuffle=False, details=True, limit=50):
-        self._build_profiles_related(limit)
-        pass
+    # NOT IN USE: normalize the profile ratios for all subreddits contained.
+    def _normalize_profile_ratios(self):
+        for profile_name, details in self.profiles.items():
+            total = sum([float(v['ratio']) for v in details.values()])
+            print(profile_name, total)
+            self.profiles_norm_ratio[profile_name] = {
+                k: float(v['ratio']) / total for k, v in details.items()}
+
+    # Normalize profile ratios only for top N subreditts.
+    # N is the "diversity" determined by base video numbers, read from base_summary
+    # Must run output_profile_base first!
+    def _normalize_profile_ratios_N_diversity(self, diversity_ratio):
+        for profile_name, details in self.profiles.items():
+            N = round(self.base_videos_summary[profile_name] * diversity_ratio)
+            temp = sorted([(k, float(v['ratio'])) for k, v in details.items()], key=lambda x: x[1], reverse=True
+                          )[:N]
+            total = sum([each[1] for each in temp])
+            self.profiles_norm_ratio_N_diversity[profile_name] = [
+                (each[0], round(each[1] / total * N)) for each in temp]
+
+    # First method to implement the extended file:
+    # Each profile consists of: 1) base profile #: max(videos_in_base_profile, top_LIMIT_in_base_profile)
+    # 2) N*# of extended videos from subreddits by <1> normalize ratios for each subreddits for top LIMIT videos
+    # <2> total # of extended videos (roughly) equals to # of base profile, choose top N videos calculated by ratio，
+    # rounded up/down to whole number
+    def output_profiles_related(self, shuffle=False, diversity_ratio=1):
+        '''
+        diversity_ratio: control the ratio of extended videos / base videos numbers
+        '''
+        print(
+            "*****This step will take long time and large memory, please hold tight!*****\n")
+        sys.stdout.flush()
+        self._build_profiles_related()
+        self._normalize_profile_ratios_N_diversity(diversity_ratio)
+        out_path = self.output_path
+        summary = {}
+        for profile_name, details in self.profiles_norm_ratio_N_diversity.items():
+            short = self.base_videos[profile_name][:]
+            for subreddit, count in details:
+                try:
+                    short.extend(self.related_videos[subreddit][:count])
+                except Exception as e:
+                    print(e)
+                    print(
+                        "Check related video folder or above subreddit name to match each other.")
+            if shuffle:
+                random.shuffle(short)
+            summary[profile_name] = len(short)
+            name_related = 'related_videos_' + profile_name + '.json'
+            self._write_json(short, os.path.join(out_path, name_related))
+        self._write_json(summary,
+                         os.path.join(out_path, 'related_summary.json'))
