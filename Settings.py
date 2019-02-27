ce9296905e321e5814c83496370068149d890498
Xiaoyu Xing <xiaoyu-xing@uiowa.edu>
2019-02-27 10:49:39 -0600

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -2,7 +2,7 @@
 
 # >>>>>>>>>>>>First section: for creating profiles<<<<<<<<<<<<
 # Hyperparameters:
-input_path = 'data'  # Input data folder
+input_path = 'data'  # Input data folder, should be the same structrue as the example
 output_path = 'output'  # Output data folder
 
 
@@ -18,8 +18,8 @@ base_detailed = False  # Generate a detailed version?
 # 1)
 by_diversity = True  # Generate by diversity_index method?
 # Choose X = (base_video_number * diversity_index number) top subreddits
-# normalize the ratios of these top X subreddits
-# choose top videos in each subreddits to
+# normalize the overlap ratios of these top X subreddits
+# do weighted sampling using normalized overlap ratios to
 # make up to total X extended videos
 diversity_index = 1.0
 # Shuffle the output for this method?
@@ -27,13 +27,14 @@ extended_shuffle_by_diversity = True
 
 # 2)
 by_random_number_generator = True  # Generate by random number method?
+
 # How many digits after decimal points for subreddits ratio:
 # e.g. ratio = 0.003, then this value should be 3
-subreddit_ratio_decimal = 3
+subreddit_ratio_decimal = 3  # the precision of ratio
 # How many percent of top videos in each subreddit will be chosen
 # Default 0.1, i.e. 10%
 sampling_percent_index = 0.1
-# Shuffle the method for this method?
+# Shuffle the output for this method?
 extended_shuffle_by_random_number = True
 
 
@@ -51,18 +52,18 @@ firefox_profile_path = ''
 seed_cookie_path = 'training/cookiebro-cookies.json'  # DO NOT OVERRIDE
 training_coockie_path = 'training/after_training_cookie.json'
 # How many seconds to watch for each video:
-watch_time = 60
+watch_time = 30
 # Initial web to visit before train, DO NOT CHANGE
 inital_website = 'https://www.youtube.com/'
 # Current account: yout317317, pass: al...b... 19800101, no gender
 # Training batch size to prevent memory overload:
-training_batch_size = 50
+training_batch_size = 20
 # full training process:
 # full traning profile name, use the reddit name, check the keys summary.json file
 # If not None, make sure training_directory is valid
 full_training_name = 'the_donald'
 # choose among 'base', 'diversity', 'RNG'
-full_training_category = 'RNG'
+full_training_category = 'base'
 # Or use the path to the full list you need to train
 full_list_path = ''  # Secondary choice, set full_training_name to None to use this
 # path to ad block extension for firefox
@@ -71,4 +72,4 @@ ad_block_path = 'adblock_for_firefox-3.25.0-an+fx.xpi'
 # Whether to play at the fastest playback speed
 fast = True
 # Report time interval, how long between report current status, unit: seconds
-report_interval = 20
+report_interval = 5 # should more than 2 seconds
