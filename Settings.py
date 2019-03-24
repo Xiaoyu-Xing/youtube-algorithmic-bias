c2093dd07652544695076983c24c032596b69c81
xx <xingxy91@gmail.com>
2019-03-24 11:00:36 -0500

    reorganize settings

--- Settings.py
+++ Settings.py
@@ -9,15 +9,13 @@
 input_path = 'data'  # Input data folder, should be the same structrue as the example
 output_path = 'output'  # Output data folder
 
-
 # For generating base files
 base = True  # Generate base files?
 base_video_number = 50  # Each profile contains # of videos
 base_shuffle = True  # Shuffle the output list?
 base_detailed = False  # Generate a detailed version?
 
-
-# For generating extended files (two methods)
+# Two methods to generate extended files
 
 # 1)
 by_diversity = True  # Generate by diversity_index method?
@@ -31,9 +29,8 @@ extended_shuffle_by_diversity = True
 
 # 2)
 by_random_number_generator = True  # Generate by random number method?
-
 # How many digits after decimal points for subreddits ratio:
-# e.g. ratio = 0.003, then this value should be 3
+# e.g. ratio = 0.001, then this value should be 3
 subreddit_ratio_decimal = 3  # the precision of ratio
 # How many percent of top videos in each subreddit will be chosen
 # Default 0.1, i.e. 10%
@@ -42,38 +39,53 @@ sampling_percent_index = 0.1
 extended_shuffle_by_random_number = True
 
 
-# >>>>>>>>>>>>Second section: for training profiles<<<<<<<<<<<<
+# >>>>>>>>>>>>Second section: for individual training profiles<<<<<<<<<<<<
 headless = False  # running in linux server need to run without GUI, then set to True
+
+# There are two training mode: 
+
+# 1) given the folder path to all of the json profiles, such as the 'sample_output' folder
+# which contains all base, extended profiles (by 2 methods),
+# then specify which reddit profile and whether base or extended need to train, set parameters below:
+
 # If you need to parse all json files
 # Give the dir to the lowest common folder below
 training_directory = 'sample_output'
-# If you only need to parse and train one list
-# Give the dir to json below
-one_path = 'training/test.json'
-# Path to a firefox profile
-firefox_profile_path = ''
-# Path to the cookies
-seed_cookie_path = 'training/cookiebro-cookies.json'  # DO NOT OVERRIDE
-training_coockie_path = 'training/after_training_cookie.json'
-# How many seconds to watch for each video:
-watch_time = 30
-# Initial web to visit before train, DO NOT CHANGE
-inital_website = 'https://www.youtube.com/'
-# Current account: yout317317, pass: al...b... 19800101, no gender
-# Training batch size to prevent memory overload:
-training_batch_size = 20
 # full training process:
 # full traning profile name, use the reddit name, check the keys summary.json file
 # If not None, make sure training_directory is valid
 full_training_name = 'the_donald'
 # choose among 'base', 'diversity', 'RNG'
 full_training_category = 'base'
-# Or use the path to the full list you need to train
+
+# 2) given a specific json file path, and only train by those videos
+# If you only need to parse and train one list
+# Give the dir to json below
 full_list_path = ''  # Secondary choice, set full_training_name to None to use this
+one_path = 'training/test.json' # This is for testing purpose, ignore in real training
+
+
+# Other parameters:
+
+# Path to a firefox profile
+firefox_profile_path = ''
+# Path to the cookies
+# Current account: yout317317@gmail.com, pass: al...b... DOB: 19800101, no gender
+seed_cookie_path = 'training/cookiebro-cookies.json'  # starter cookie before training
+training_coockie_path = 'training/after_training_cookie.json' # where to save cookie after training
+# How many seconds to watch for each video:
+watch_time = 30
+# Initial web to visit before train, DO NOT CHANGE unless for other projects
+inital_website = 'https://www.youtube.com/'
+# Training batch size to prevent memory overload:
+training_batch_size = 20
 # path to ad block extension for firefox
-# adblock pro is the only one found so far not pop up new page after installation!
 ad_block_path = 'adblock_for_firefox-3.25.0-an+fx.xpi'
 # Whether to play at the fastest playback speed
 fast = True
 # Report time interval, how long between report current status, unit: seconds
 report_interval = 5 # should more than 2 seconds
+
+
+
+# >>>>>>>>>>>>Third section: for training in parallel mode<<<<<<<<<<<<
