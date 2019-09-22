4ab712699ac034cdb017e84a1bd3159210000de1
xx <xingxy91@gmail.com>
2019-09-22 10:55:22 -0500

    try a map_async to solve early termination of browser process in stateless training

--- src/experimenting_field/stateful_pilot_test_driver.py
+++ src/experimenting_field/stateful_pilot_test_driver.py
@@ -1,16 +1,12 @@
-import logging
 import os
 from multiprocessing import Pool
 from typing import List
 
 import settings
-from src.common_utils.LogSetup import setup_log
 from src.experimenting_field.StatefulTrainAndTest import stateful_train_and_test
 
 
 def start_stateful():
-    # setup_log(os.path.basename(str(__file__).strip(".py")) + str(os.getpid()))
-    # log = logging.getLogger(__name__ + str(os.getpid()))
     input_video_parent_path: str = \
         os.path.join(settings.ROOT_DIR, settings.INPUT_DATA, "stateful_videos")
     donald_path: str = os.path.join(input_video_parent_path, "related_videos_RNG_the_donald.json")
@@ -20,12 +16,10 @@ def start_stateful():
     cookie_base_path: str = os.path.join(settings.ROOT_DIR, settings.INPUT_DATA, "cookies")
     cookie_path: List[str] = [os.path.join(cookie_base_path, file_name) for file_name in
                               ["the_donald.json", "enoughtrumpspam.json", "blank.json"]]
-    # log.info("Videos to be visited: {}".format(video_list))
-    # log.info("Cookies to be loaded: {}.".format(cookie_path))
     print("Videos to be visited: {}".format(video_list))
     print("Cookies to be loaded: {}.".format(cookie_path))
-    with Pool(processes=len(video_list)) as pool:
-        pool.starmap(stateful_train_and_test, zip(video_list, cookie_path))
+    with Pool(processes=len(video_list), maxtasksperchild=1) as pool:
+        pool.starmap_async(stateful_train_and_test, zip(video_list, cookie_path))
 
 
 if __name__ == "__main__":
--- src/experimenting_field/stateless_pilot_test_driver.py
+++ src/experimenting_field/stateless_pilot_test_driver.py
@@ -1,16 +1,12 @@
-import logging
 import os
 from multiprocessing import Pool
 from typing import List
 
 import settings
-from src.common_utils.LogSetup import setup_log
 from src.experimenting_field.StatelessTrainAndTest import stateless_train_and_test
 
 
 def start_stateless():
-    # setup_log(os.path.basename(str(__file__).strip(".py")) + str(os.getpid()))
-    # log = logging.getLogger(__name__ + str(os.getpid()))
     stateless_videos: str = "stateless_videos"
     input_video_parent_path: str = \
         os.path.join(settings.ROOT_DIR, settings.INPUT_DATA, stateless_videos)
@@ -18,10 +14,9 @@ def start_stateless():
     enoughtrumpspam_path: str = os.path.join(input_video_parent_path,
                                              "related_videos_RNG_enoughtrumpspam.json")
     video_list: List[str] = [donald_path, enoughtrumpspam_path, ""]
-    # log.info("Videos to be visited: {}".format(video_list))
     print("Videos to be visited: {}".format(video_list))
-    with Pool(processes=len(video_list)) as pool:
-        pool.map(stateless_train_and_test, video_list)
+    with Pool(processes=len(video_list), maxtasksperchild=1) as pool:
+        pool.map_async(stateless_train_and_test, video_list)
 
 
 if __name__ == "__main__":
