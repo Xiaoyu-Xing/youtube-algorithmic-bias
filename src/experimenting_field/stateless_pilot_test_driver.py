2fbdc8671907ce6b9d5269c74d9b282f84c483f8
Xiaoyu Xing <xingxy91@gmail.com>
2019-09-26 22:11:26 -0400

    change new process start method so firefox broswers and displays won't share the same pid

--- src/experimenting_field/stateless_pilot_test_driver.py
+++ src/experimenting_field/stateless_pilot_test_driver.py
@@ -1,5 +1,6 @@
 import os
-from concurrent.futures import ProcessPoolExecutor as Pool
+# from concurrent.futures import ProcessPoolExecutor as Pool
+import multiprocessing as mp
 from typing import List
 
 import settings
@@ -15,9 +16,10 @@ def start_stateless():
                                              "related_videos_RNG_enoughtrumpspam.json")
     video_list: List[str] = [donald_path, enoughtrumpspam_path, ""]
     print("Videos to be visited: {}".format(video_list))
-    with Pool() as pool:
+    with mp.Pool() as pool:
         pool.map(stateless_train_and_test, video_list)
 
 
 if __name__ == "__main__":
+    mp.set_start_method("spawn")
     start_stateless()
